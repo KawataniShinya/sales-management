6fa018fd5cf1833615080f9ed74d29609dc6e739
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-10 00:58:44 +0900

    display hello world

---
+++ src/main/java/com/searchEmployee/Login.java
@@ -0,0 +1,12 @@
+package com.searchEmployee;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Login {
+    @RequestMapping("/Login")
+    private String login(){
+        return "/index.html";
+    }
+}
