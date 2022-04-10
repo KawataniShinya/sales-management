ea7b7fbb5a3bc56ff14384dcc7fbf0d8dd6ec5c5
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-10 12:36:54 +0900

    add Thymeleaf

--- src/main/java/com/searchEmployee/Login.java
+++ src/main/java/com/searchEmployee/Login.java
@@ -1,12 +1,22 @@
 package com.searchEmployee;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Login {
-    @RequestMapping("/Login")
+    @RequestMapping("/login")
     private String login(){
-        return "/index.html";
+        return "login.html";
+    }
+
+    @RequestMapping("/output")
+    private String result(@RequestParam String id, Model model){
+        String name = "コントローラー太郎";
+        model.addAttribute("id", id);
+        model.addAttribute("name", name);
+        return "output.html";
     }
 }
