247bb5098e2037ec1acb18c9000d1e9b3ea10dbd
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-22 23:01:58 +0900

    add logged in redirect

--- src/main/java/com/search/Login.java
+++ src/main/java/com/search/Login.java
@@ -2,8 +2,10 @@ package com.search;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,20 +13,22 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class Login {
     @RequestMapping("/login")
-    private String login(HttpServletRequest request, Model model){
+    private String login(HttpServletRequest request, Model model, RedirectAttributes redirectAttributes){
         HttpSession session = request.getSession(false);
-        System.out.println(session);
+
         if (session != null) {
-            model.addAttribute("id", session.getAttribute("id"));
-            model.addAttribute("password", session.getAttribute("password"));
+            redirectAttributes.addFlashAttribute("id", session.getAttribute("id"));
+            redirectAttributes.addFlashAttribute("pass", session.getAttribute("password"));
+            return "redirect:/redirectResult";
         }
+
         return "login.html";
     }
 
-    @RequestMapping("/result")
+    @PostMapping("/result")
     private String result(HttpServletRequest request, @RequestParam String id, String pass, Model model){
+
         HttpSession session = request.getSession(true);
-        System.out.println(session);
 
         String name = "";
         if (id.equals("root") && pass.equals("password")){
@@ -41,6 +45,17 @@ public class Login {
 
         model.addAttribute("id", id);
         model.addAttribute("name", name);
+
+        return "result.html";
+    }
+
+    @RequestMapping("/redirectResult")
+    private String redirectResult(HttpServletRequest request, Model model){
+
+        HttpSession session = request.getSession(false);
+        model.addAttribute("status", "ログイン成功");
+        model.addAttribute("id", session.getAttribute("id"));
+        model.addAttribute("name", session.getAttribute("name"));
 
         return "result.html";
     }
