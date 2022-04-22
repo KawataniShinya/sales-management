d439ef580ac74c387e97d68a5fedd61b753c5e66
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-22 20:52:01 +0900

    add session

--- src/main/java/com/search/Login.java
+++ src/main/java/com/search/Login.java
@@ -5,15 +5,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class Login {
     @RequestMapping("/login")
-    private String login(){
+    private String login(HttpServletRequest request, Model model){
+        HttpSession session = request.getSession(false);
+        System.out.println(session);
+        if (session != null) {
+            model.addAttribute("id", session.getAttribute("id"));
+            model.addAttribute("password", session.getAttribute("password"));
+        }
         return "login.html";
     }
 
     @RequestMapping("/result")
-    private String result(@RequestParam String id, String pass, Model model){
+    private String result(HttpServletRequest request, @RequestParam String id, String pass, Model model){
+        HttpSession session = request.getSession(true);
+        System.out.println(session);
+
         String name = "";
         if (id.equals("root") && pass.equals("password")){
             name = "ログイン太郎";
@@ -22,8 +34,14 @@ public class Login {
             name = "no name";
             model.addAttribute("status", "ログイン失敗");
         }
+
+        session.setAttribute("id", id);
+        session.setAttribute("name", name);
+        session.setAttribute("password", pass);
+
         model.addAttribute("id", id);
         model.addAttribute("name", name);
+
         return "result.html";
     }
 }
