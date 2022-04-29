aa6d44b1dab18bfb6e46a1888c84e0449280c8e5
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-04-29 17:52:08 +0900

    make sql define external properties file

--- src/main/java/com/search/SerchEmployee.java
+++ src/main/java/com/search/SerchEmployee.java
@@ -2,6 +2,8 @@ package com.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
+@PropertySource(value = "classpath:properties/sql.properties")
 public class SerchEmployee {
     @Autowired
     private JdbcTemplate jdbcTemplate1;
@@ -27,13 +30,20 @@ public class SerchEmployee {
     @Autowired @Qualifier("secondaryNpjdbc")
     private NamedParameterJdbcTemplate npJdbcTemplate2;
 
+    @Value("${SQLL001}")
+    String sql11;
+    @Value("${SQLL002}")
+    String sql12;
+    @Value("${SQLA001}")
+    String sql21;
+    @Value("${SQLA002}")
+    String sql22;
+
     @RequestMapping("/serch")
     private String serch(){
-        String sql11 = "SELECT * FROM shain_table";
         List<Map<String, Object>> list11 = jdbcTemplate1.queryForList(sql11);
         System.out.println("sql11 : " + list11);
 
-        String sql12 = "SELECT * FROM shain_table where id<>:ID and nen<>:NEN";
         Map<String, String> paramMap1 = new HashMap<String, String>();
         paramMap1.put("ID", "100");
         paramMap1.put("NEN", "2003");
@@ -41,11 +51,9 @@ public class SerchEmployee {
         System.out.println("sql12 : " + list12);
 
 
-        String sql21 = "SELECT * FROM t_mst_employee";
         List<Map<String, Object>> list21 = jdbcTemplate2.queryForList(sql21);
         System.out.println("sql21 : " + list21);
 
-        String sql22 = "SELECT * FROM t_mst_employee where id<>:ID and nen<>:NEN and sei=:SEI";
         Map<String, Object> paramMap2 = new HashMap<String, Object>();
         paramMap2.put("ID", 101);
         paramMap2.put("NEN", 2005);
