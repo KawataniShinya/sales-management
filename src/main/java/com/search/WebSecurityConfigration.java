7ac0c32654fc76425c1efd721ec1af3b30d53256
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-05-01 17:24:32 +0900

    implement bcrypt step1 (encrypt password in memory)

--- src/main/java/com/search/WebSecurityConfigration.java
+++ src/main/java/com/search/WebSecurityConfigration.java
@@ -1,13 +1,20 @@
 package com.search;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfigration extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -35,6 +42,6 @@ public class WebSecurityConfigration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("root").password("{noop}zaq12wsx").roles("USER");
+        auth.inMemoryAuthentication().withUser("root").password("{bcrypt}$2a$08$uGieQ2B6N9Ic7666NIpgI.7I70oits21rmd3D1M1nhBWnG8Zsko7C").roles("USER");
     }
 }
