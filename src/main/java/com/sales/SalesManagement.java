4201345b09b2e1acb450ecb93c8c4f2e9037bbcb
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-06-09 15:11:40 +0900

    add feature redirect when 404

--- src/main/java/com/sales/SalesManagement.java
+++ src/main/java/com/sales/SalesManagement.java
@@ -2,17 +2,11 @@ package com.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SalesManagement {
-
-	@RequestMapping("/")
-	String index(){
-		return "Welcome to Test Application";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesManagement.class, args);
