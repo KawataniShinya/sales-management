535c0c81eabb2c93153a6247fd35a240e3a60ef4
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-06-17 18:59:28 +0900

    refactoring staff search controller

--- src/main/java/com/sales/presentation/StaffController.java
+++ src/main/java/com/sales/presentation/StaffController.java
@@ -6,6 +6,7 @@ import com.sales.common.DomainRuleIllegalException;
 import com.sales.common.Errors;
 import com.sales.domain.staff.Constant;
 import com.sales.presentation.dto.StaffControllerGetStaffsRequest;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -19,10 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.awt.desktop.SystemEventListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,36 +46,44 @@ public class StaffController {
     public String getStaffs(HttpServletRequest request, Model model, @ModelAttribute @Validated StaffControllerGetStaffsRequest param, BindingResult result) {
         CommonDisplay.setHeaderParameter(request, model);
 
-        Errors errors = new Errors();
+//        param.setLimitSize(25);
+//        param.setPage(4);
+//        param.setUserId("STF0000081");
+//        param.setUserName("瀬菜");
+//        param.setDepartmentCd("D10001");
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        try {
+//            param.setParamExpirationStart(simpleDateFormat.parse("2022-06-01"));
+//            param.setParamExpirationEnd(simpleDateFormat.parse("2022-09-01"));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
 
-        for (ObjectError error : result.getAllErrors()) {
-            if (error instanceof FieldError) {
-                errors.getField().put(((FieldError) error).getField(), error.getDefaultMessage());
-            }
-        }
-        if (!errors.getField().isEmpty()) {
-            model.addAttribute(com.sales.presentation.Constant.RESPONSE_COMMON.FIELD_ERROR_MESSAGES.getValue(), errors.getField());
-//            return "staff-search.html";
-        }
-//        errors.getGlobal().add("global message test 1");
-//        errors.getGlobal().add("global message test 2");
-//        errors.getGlobal().add("global message test 3");
-//        errors.getGlobal().add("global message test 4");
+        if (param.getPage() > 0) {
+            Errors errors = new Errors();
 
-        Map<String, Object> map = new HashMap<>();
-        map.put(Constant.API_SEARCH_PARAM_STAFF.LIMIT_SIZE.getValue(), 25);
-        map.put(Constant.API_SEARCH_PARAM_STAFF.PAGE.getValue(), 4);
-        map.put(Constant.API_SEARCH_PARAM_STAFF.USER_ID.getValue(), "STF0000081");
-        map.put(Constant.API_SEARCH_PARAM_STAFF.USER_NAME.getValue(), "瀬菜");
-        map.put(Constant.API_SEARCH_PARAM_STAFF.DEPARTMENT_CD.getValue(), "D10001");
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            map.put(Constant.API_SEARCH_PARAM_STAFF.PARAM_EXPIRATION_START.getValue(), simpleDateFormat.parse("2022-09-02"));
-            map.put(Constant.API_SEARCH_PARAM_STAFF.PARAM_EXPIRATION_END.getValue(), simpleDateFormat.parse("2022-09-01"));
-        } catch (ParseException e) {
-            e.printStackTrace();
+            if (!checkBeanValidationAndSetErrorMessages(model, result, errors)) return "staff-search.html";
+
+            Map<String, Object> map = new HashMap<>();
+            setFindStaffsParameter(param, map);
+
+            StaffServiceBean staffServiceBean = getStaffServiceBeanOrSetErrorMessages(model, errors, map);
+            if (staffServiceBean == null) return "staff-search.html";
+
+            setResultAsAttribute(model, staffServiceBean);
         }
 
+        return "staff-search.html";
+    }
+
+    private void setResultAsAttribute(Model model, StaffServiceBean staffServiceBean) {
+        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.STAFFS.getValue(), staffServiceBean.getStaffs());
+        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.COUNT.getValue(), staffServiceBean.getCount());
+        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.LIMIT_SIZE.getValue(), staffServiceBean.getLimitSize());
+        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.PAGE.getValue(), staffServiceBean.getPage());
+    }
+
+    private StaffServiceBean getStaffServiceBeanOrSetErrorMessages(Model model, Errors errors, Map<String, Object> map) {
         StaffServiceBean staffServiceBean = null;
         try {
             staffServiceBean = this.staffService.findStaffs(map);
@@ -84,14 +92,31 @@ public class StaffController {
         }
         if (!errors.getGlobal().isEmpty()) {
             model.addAttribute(com.sales.presentation.Constant.RESPONSE_COMMON.GLOBAL_ERROR_MESSAGES.getValue(), errors.getGlobal());
-            return "staff-search.html";
         }
+        return staffServiceBean;
+    }
 
-        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.STAFFS.getValue(), staffServiceBean.getStaffs());
-        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.COUNT.getValue(), staffServiceBean.getCount());
-        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.LIMIT_SIZE.getValue(), staffServiceBean.getLimitSize());
-        model.addAttribute(Constant.API_SEARCH_PARAM_STAFF.PAGE.getValue(), staffServiceBean.getPage());
+    private void setFindStaffsParameter(StaffControllerGetStaffsRequest param, Map<String, Object> map) {
+        map.put(Constant.API_SEARCH_PARAM_STAFF.LIMIT_SIZE.getValue(), param.getLimitSize());
+        map.put(Constant.API_SEARCH_PARAM_STAFF.PAGE.getValue(), param.getPage());
+        map.put(Constant.API_SEARCH_PARAM_STAFF.USER_ID.getValue(), param.getUserId());
+        map.put(Constant.API_SEARCH_PARAM_STAFF.USER_NAME.getValue(), param.getUserName());
+        map.put(Constant.API_SEARCH_PARAM_STAFF.DEPARTMENT_CD.getValue(), param.getDepartmentCd());
+        map.put(Constant.API_SEARCH_PARAM_STAFF.PARAM_EXPIRATION_START.getValue(), DateUtils.truncate(param.getParamExpirationStart(), Calendar.DAY_OF_MONTH));
+        map.put(Constant.API_SEARCH_PARAM_STAFF.PARAM_EXPIRATION_END.getValue(), DateUtils.truncate(param.getParamExpirationStart(), Calendar.DAY_OF_MONTH));
+    }
 
-        return "staff-search.html";
+    private boolean checkBeanValidationAndSetErrorMessages(Model model, BindingResult result, Errors errors) {
+        boolean isPassed = true;
+        for (ObjectError error : result.getAllErrors()) {
+            if (error instanceof FieldError) {
+                errors.getField().put(((FieldError) error).getField(), error.getDefaultMessage());
+            }
+        }
+        if (!errors.getField().isEmpty()) {
+            model.addAttribute(com.sales.presentation.Constant.RESPONSE_COMMON.FIELD_ERROR_MESSAGES.getValue(), errors.getField());
+            isPassed = false;
+        }
+        return isPassed;
     }
 }
--- src/main/java/com/sales/presentation/dto/StaffControllerGetStaffsRequest.java
+++ src/main/java/com/sales/presentation/dto/StaffControllerGetStaffsRequest.java
@@ -2,10 +2,12 @@ package com.sales.presentation.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -14,4 +16,11 @@ public class StaffControllerGetStaffsRequest {
     private int limitSize;
     @Positive
     private int page;
+    private String userId;
+    private String userName;
+    private String departmentCd;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date paramExpirationStart;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date paramExpirationEnd;
 }
