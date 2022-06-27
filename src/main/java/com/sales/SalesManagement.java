717a1824127bb821f23b305f67c2b3b18c2cf45c
Kawatani Shinya <peritrochoidonsilverfox@gmail.com>
2022-06-27 16:45:39 +0900

    adjustment to deploy local tomcat

--- src/main/java/com/sales/SalesManagement.java
+++ src/main/java/com/sales/SalesManagement.java
@@ -2,14 +2,20 @@ package com.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SalesManagement {
+public class SalesManagement extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesManagement.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SalesManagement.class);
+	}
 }
